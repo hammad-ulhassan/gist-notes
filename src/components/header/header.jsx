55ceafffa4d2170c0e6baff2d94b796c90bed98b
Hammad <macbook@192.168.10.5>
2022-05-10 21:14:48 +0500

    initial commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "./logo/logo";
+import './header.css'
+import { Input } from 'antd';
+import { Button } from 'antd';
+import { SearchOutlined} from '@ant-design/icons';
+
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="header-style emumba-green">
+        <Logo fillColor="#ffffff" />
+        <div className="flex-row">
+          <Input 
+            placeholder="Search Notes..." 
+            bordered={true} 
+            suffix={
+                <SearchOutlined />
+            }
+            />
+          <Button>Login</Button>
+        </div>
+      </header>
+    );
+  }
+}
---
+++ src/components/main/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./main.css";
+import { Button } from "antd";
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className="main-offset">
+          <div className="container"></div>
+        </div>
+      </main>
+    );
+  }
+}
